bc22cc351c1dbb6220b56052a25719337b99a889
thesamtang <linglinghost@gmail.com>
2015-02-28 13:25:14 -0500

    add support for short hex codes

--- js/color.js
+++ js/color.js
@@ -2,25 +2,28 @@ $(document).ready(function() {
     "use strict";
     var $stage = $("#stage"),
         $hex = $("#hex"),
-        hash = "#",
-        colorStack = ["cccccc"];
+        hash = "#";
+    $.colorStack = ["#CCCCCC"];
 
     $.changeColor = function(hexcode) {
-        if (hexcode.length === 6) {
-            //colorStack.push(hexcode);
-            var hashed = hash.concat(hexcode);
-            $stage.css({"background-color": hashed});
-            $hex.css({"background-color": hashed});
-            console.log(colorStack);
-        }
+        $.colorStack.push(hexcode);
+        $stage.css({"background-color": hexcode});
+        $hex.css({"background-color": hexcode});
+        console.log($.colorStack);
     };
 
     $hex.keyup(function(event) {
-        console.log("keyup");
-        var hex = $hex.val();
-        if (hex.length === 6) {
-            $.changeColor(hex);
+        var hexVal = hash.concat($hex.val().toUpperCase());
+        if ($.isValidHex(hexVal) && hexVal !== $.colorStack[$.colorStack.length - 1]) {
+            $.changeColor(hexVal);
+            console.log(hexVal);
         }
     });
 });
+
+$.hexRegex = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/;
+
+$.isValidHex = function(hexcode) {
+    return $.hexRegex.test(hexcode);
+}
 
--- test/colorTest.js
+++ test/colorTest.js
@@ -1,35 +1,66 @@
 QUnit.test("Set stage color", function(assert) {
     var valid = "cccccc",
         invalid = "cccccj",
-        short = "abc",
+        shortValid = "abc",
+        shortInvalid = "ggg",
         long = "ccccccc",
         $stage = $("#stage"),
         $hex = $("#hex");
 
-    $stage.css("height", "10px");
     $stage.css("background-color", "#aaaaaa");
+
+    $hex.val(valid);
+    $hex.keyup();
+    assert.equal($stage.css("background-color"), "#cccccc");
+    assert.equal($hex.css("background-color"), "#cccccc");
+    console.log(1);
 
     $hex.val(invalid);
     $hex.keyup();
-    assert.equal($stage.css("background-color"), "#aaaaaa");
+    assert.equal($stage.css("background-color"), "#cccccc");
+    assert.equal($hex.css("background-color"), "#cccccc");
+    console.log(2);
 
-    $hex.val(short);
+    $hex.val(shortValid);
     $hex.keyup();
-    assert.equal($stage.css("background-color"), "#aaaaaa");
+    assert.equal($stage.css("background-color"), "#aabbcc");
+    assert.equal($hex.css("background-color"), "#aabbcc");
+    console.log(3);
+
+    $hex.val(shortInvalid);
+    $hex.keyup();
+    assert.equal($stage.css("background-color"), "#aabbcc");
+    assert.equal($hex.css("background-color"), "#aabbcc");
+    console.log(4);
 
     $hex.val(long);
     $hex.keyup();
-    assert.equal($stage.css("background-color"), "#aaaaaa");
-
-    $hex.val(valid);
-    $hex.trigger("keyup");
-    assert.equal($stage.css("background-color"), "#cccccc");
-
+    assert.equal($stage.css("background-color"), "#aabbcc");
+    assert.equal($hex.css("background-color"), "#aabbcc");
+    console.log(5);
 });
 
-//QUnit.test("Increase color stack size", function(assert){
-//
-//});
+QUnit.test("Increase color stack size", function(assert){
+
+    var $stage = $("#stage"),
+        $hex = $("#hex");
+
+    $hex.val("cccccc");
+    $hex.keyup();
+    var len = $.colorStack.length;
+
+    // Add same color
+    $hex.val("cccccc");
+    $hex.keyup();
+    assert.equal($.colorStack.length, len);
+    console.log(6);
+
+    // Add new color
+    $hex.val("aaaaaa");
+    $hex.keyup();
+    assert.equal($.colorStack.length, len + 1);
+    console.log(7);
+});
 
 // test for hex color change for light and dark bkgds
 
